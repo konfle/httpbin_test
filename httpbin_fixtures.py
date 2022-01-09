a4a2bed7e621ed51994f388ecd14bf403a84ba4b
konfle <konrad.flejter@gmail.com>
2022-01-09 22:43:18 +0100

    refactor validate_orogin_ip

--- httpbin_fixtures.py
+++ httpbin_fixtures.py
@@ -1,7 +1,13 @@
 import requests
 import pytest
 
+ip_addresses = ["10.32.3",
+                "192.168.0.1.2",
+                "192.168.10.256",
+                "10.58.15.-1"]
 
+
+@pytest.fixture
 def origin_ip():
     url = "https://httpbin.org/ip"
 
@@ -19,3 +25,16 @@ def origin_user_agent():
     user_agent = response.json()
 
     return user_agent["user-agent"]
+
+
+def origin_ip_without_fixutre():
+    url = "https://httpbin.org/ip"
+
+    response = requests.request("GET", url)
+    ip = response.json()
+
+    return ip["origin"]
+
+
+user_ip = origin_ip_without_fixutre()
+ip_addresses.insert(0, user_ip)
